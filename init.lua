1af369e2dba9f23c26a34d03de5a4188f6e2695e
Adam Comer <adambcomer@gmail.com>
2023-10-25 01:34:55 -0600

    Remapped window movement

--- init.lua
+++ init.lua
@@ -167,3 +167,9 @@ vim.opt.incsearch = true
 
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
+
+-- Window Navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
