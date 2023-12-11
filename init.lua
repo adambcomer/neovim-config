9a5dd715f57deb0ca25fe786052c7dc2f38f3be2
Adam Comer <adambcomer@gmail.com>
2023-12-10 18:25:23 -0700

    Added conform and updated packages

--- init.lua
+++ init.lua
@@ -1,113 +1,146 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  { 'catppuccin/nvim',          name = 'catppuccin', priority = 1000 },
-  { 'nvim-lualine/lualine.nvim' },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      local configs = require('nvim-treesitter.configs')
+require("lazy").setup({
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "nvim-lualine/lualine.nvim" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'html', 'css', 'typescript', 'go', 'markdown' },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.3',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-      { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
-      { '<leader>fg', '<cmd>Telescope live_grep<cr>',  desc = 'Find Text' },
-      { '<leader>fb', '<cmd>Telescope buffers<cr>',    desc = 'Find Buffers' },
-      { '<leader>fh', '<cmd>Telescope help_tabs<cr>',  desc = 'Find Help' }
-    },
-  },
-  {
-    'nvim-telescope/telescope-file-browser.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
-  },
-  { 'williamboman/mason.nvim' },
-  { 'williamboman/mason-lspconfig.nvim' },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-    },
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      { 'L3MON4D3/LuaSnip' },
-    }
-  },
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim',         -- required
-      'nvim-telescope/telescope.nvim', -- optional
-      'sindrets/diffview.nvim',        -- optional
-      'ibhagwan/fzf-lua',              -- optional
-    },
-    config = true,
-    keys = {
-      { '<leader>g', '<cmd>Neogit<cr>', desc = 'Open Git Window' }
-    }
-  },
-  {
-    'lewis6991/gitsigns.nvim'
-  }
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"html",
+					"css",
+					"typescript",
+					"go",
+					"markdown",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.3",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find Text" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
+			{ "<leader>fh", "<cmd>Telescope help_tabs<cr>", desc = "Find Help" },
+		},
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "L3MON4D3/LuaSnip" },
+		},
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"nvim-telescope/telescope.nvim", -- optional
+			"sindrets/diffview.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+		keys = {
+			{ "<leader>g", "<cmd>Neogit<cr>", desc = "Open Git Window" },
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+	},
+	{
+		"stevearc/conform.nvim",
+	},
 })
 
 ---
 -- LSP setup
 ---
-require('config.lsp')
+require("config.lsp")
 
 ---
 -- Git Signs
 ---
-require('config.gitsigns')
+require("config.gitsigns")
 
 ---
 -- Lualine
 ---
-require('lualine').setup()
+require("lualine").setup()
 
 ---
 -- Telescope
 ---
-require('telescope').setup {
-  extensions = {
-    file_browser = {
-      hijack_netrw = true
-    }
-  }
-}
-require('telescope').load_extension('file_browser')
+require("telescope").setup({
+	extensions = {
+		file_browser = {
+			hijack_netrw = true,
+		},
+	},
+})
+require("telescope").load_extension("file_browser")
+
+---
+-- Conform Formatter
+---
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { { "prettierd", "prettier" } },
+		html = { { "prettierd", "prettier" } },
+		css = { { "prettierd", "prettier" } },
+		go = { "goimports", "gofmt" },
+		c = { "clang-format" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+})
 
 ---
 -- Nvim Keybindings and Config
 ---
-vim.cmd.colorscheme 'catppuccin-mocha'
+vim.cmd.colorscheme("catppuccin-mocha")
 
 vim.opt.termguicolors = true
 
@@ -126,13 +159,13 @@ vim.opt.wrap = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.spelllang = 'en_us'
+vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
 -- Window Navigation
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 
-vim.keymap.set('n', '<leader>fb', '<cmd>:Telescope file_browser<cr>')
+vim.keymap.set("n", "<leader>fb", "<cmd>:Telescope file_browser<cr>")
