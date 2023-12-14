e1fa278d927b494446f452b2abb35a9bb156b209
adambcomer <adam.comer@syniti.com>
2023-12-13 17:09:48 -0700

    Added typescript formatting and updated packages

--- init.lua
+++ init.lua
@@ -126,6 +126,8 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		javascript = { { "prettierd", "prettier" } },
+		typescript = { { "prettierd", "prettier" } },
+		typescriptreact = { { "prettierd", "prettier" } },
 		html = { { "prettierd", "prettier" } },
 		css = { { "prettierd", "prettier" } },
 		go = { "goimports", "gofmt" },
